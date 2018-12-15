f9a0072a9bd65d95ac07ed97b7d296ed118de6d8
dj0mlinho <30881187+dj0mlinho@users.noreply.github.com>
2018-12-15 18:46:27 +0100

    final.second

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ let light = document.querySelectorAll('.light');
 
 function turnOn() {
   light[0].style.background = "red"
-  light[1].style.background = "none"
-  light[2].style.background = "none"
+  light[1].style.background = "#111"
+  light[2].style.background = "#111"
   setTimeout(() => {
     light[0].style.background = "red"
     light[1].style.background = "yellow"
-    light[2].style.background = "none"
+    light[2].style.background = "#111"
   }, 3000);
   setTimeout(() => {
-    light[0].style.background = "none"
-    light[1].style.background = "none"
+    light[0].style.background = "#111"
+    light[1].style.background = "#111"
     light[2].style.background = "green"
   }, 4000);
 }
